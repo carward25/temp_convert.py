1e708a22ceb7a00155e62918e48205c1192485c4
carward25 <carward@ucsc.edu>
2021-10-06 21:47:40 -0700

    #This is the project for assignment 2 part b

---
+++ temp_convert.py
@@ -0,0 +1,11 @@
+#This is a program that converts Celsius to Fahrenheit
+
+C = float(input("Please input a Celsius temperature: "))
+#Calculation
+num1 = int(9)
+num2 = int(5)
+num3 = int(32)
+F = ((num1/num2)*C) + num3
+
+# Print Fahrenheit temperature
+print("The equivalent Fahrenheit temperature is:", F)
